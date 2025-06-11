89ad85ccac7d0bebcad041b14f636adc6592c2b0
Zuza Zaremska <147152382+zuza-zaremska-epm@users.noreply.github.com>
2025-06-11 17:33:02 +0200

    Update streamlitt_app.py

--- streamlitt_app.py
+++ streamlitt_app.py
@@ -26,9 +26,10 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     st.write(ingredients_list)
-    ingredients_string = ", ".join(ingredients_list)
+    ingredients_string = ""
   
     for fruit in ingredients_list:
+      ingredients_string += fruit + " "
       search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0]
       # st.write('The search value for ', fruit,' is ', search_on, '.')
       
