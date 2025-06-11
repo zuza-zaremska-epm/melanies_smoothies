26fea9182be8e02addf1c6620ee8f4dea4344814
Zuza Zaremska <147152382+zuza-zaremska-epm@users.noreply.github.com>
2025-06-11 16:41:48 +0200

    Update streamlitt_app.py

--- streamlitt_app.py
+++ streamlitt_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -21,6 +21,8 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5
 )
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 if ingredients_list:
     st.write(ingredients_list)
