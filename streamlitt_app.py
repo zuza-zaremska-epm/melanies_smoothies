c46675e0eb76351c4f9af6e481a9125113ebb636
Zuza Zaremska <147152382+zuza-zaremska-epm@users.noreply.github.com>
2025-06-11 16:45:04 +0200

    Update streamlitt_app.py

--- streamlitt_app.py
+++ streamlitt_app.py
@@ -23,7 +23,7 @@ ingredients_list = st.multiselect(
 )
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothie_froot.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 if ingredients_list:
     st.write(ingredients_list)
